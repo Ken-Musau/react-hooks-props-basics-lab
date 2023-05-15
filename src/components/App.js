81f229e9486b58dcdc9d535aff33bde9e283ccfa
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-15 21:19:41 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color = 
+      {user.color} name = {user.name} city = {user.city}/>
+      <About links= {user.links} bio ={user.bio}/>
     </div>
   );
 }
